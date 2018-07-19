d4bea7474467b1006f97054f1f475491ac9cd88c
emmi1597 <35738015+emmi1597@users.noreply.github.com>
2018-07-19 09:09:05 -0500

    Add files via upload

---
+++ modules/sales.php
@@ -0,0 +1,19 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            Tablero
+            <small>Panel de control</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i>Inicio</a></li>
+            <li class="active">Ventas</li>
+        </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        
+    </section>
+    <!-- /.content -->
+</div>
