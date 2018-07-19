c1e4b99a315262c90a5182c136c6ee55be3a6250
emmi1597 <35738015+emmi1597@users.noreply.github.com>
2018-07-19 09:08:33 -0500

    Add files via upload

---
+++ template.php
@@ -0,0 +1,89 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Inventory System</title>
+
+  <link rel="icon" type="text/css" href="views/img/template/icono-negro.png">
+
+  <!-- PLUGINS DE CSS -->
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="views/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="views/bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="views/bower_components/Ionicons/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="views/dist/css/AdminLTE.css">
+  <!-- AdminLTE Skins -->
+  <link rel="stylesheet" href="views/dist/css/skins/_all-skins.min.css">
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+  <!-- PLUGINS DE JS -->
+  <!-- jQuery 3 -->
+  <script src="views/bower_components/jquery/dist/jquery.min.js"></script>
+  <!-- Bootstrap 3.3.7 -->
+  <script src="views/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+  <!-- SlimScroll -->
+  <script src="views/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+  <!-- FastClick -->
+  <script src="views/bower_components/fastclick/lib/fastclick.js"></script>
+  <!-- AdminLTE App -->
+  <script src="views/dist/js/adminlte.min.js"></script>
+
+</head>
+<body class="hold-transition skin-blue sidebar-collapse sidebar-mini login-page">
+  <?php if (isset($_SESSION['iniciarSesion']) && $_SESSION['iniciarSesion'] == 'ok') { ?>
+    <!-- Site wrapper -->
+    <div class="wrapper">
+
+      <!-- Header. contains the nav menu. -->
+      <?php include 'modules/header.php' ?>
+
+      <!-- Left side column. contains the sidebar -->
+      <?php include 'modules/menu.php' ?>
+
+      <!-- Content Wrapper. Contains page content -->
+      <?php
+        if (isset($_GET['ruta'])) {
+          if ($_GET['ruta'] == 'home' ||
+              $_GET['ruta'] == 'users' ||
+              $_GET['ruta'] == 'categories' ||
+              $_GET['ruta'] == 'products' ||
+              $_GET['ruta'] == 'customers' ||
+              $_GET['ruta'] == 'sales' ||
+              $_GET['ruta'] == 'sales-create' ||
+              $_GET['ruta'] == 'sales-report' ||
+              $_GET['ruta'] == 'logout'
+            ) {
+            include 'modules/'.$_GET['ruta'].'.php';
+          } else {
+            include 'modules/404.php';
+          }
+        } else {
+          include 'modules/home.php';
+        }
+      ?>
+
+      <?php include 'modules/footer.php' ?>
+
+    </div>
+    <!-- ./wrapper -->
+  <?php } else { ?>
+
+    <!-- Login. contains the login page. -->
+    <?php include 'modules/login.php' ?>
+
+  <?php } ?>
+<script src="views/js/template.js"></script>
+</body>
+</html>
